96c168cd604026c0c5dbe9b5676fec7bddec96c3
anikievgr <anikiev.gleb.2005@yandex.ru>
2023-01-06 12:54:18 +0300

    now

--- example-app/resources/views/pagehome.blade.php
+++ example-app/resources/views/pagehome.blade.php
@@ -73,7 +73,7 @@
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">
                         Занимаем почти 90% словацкого рынка
                     </div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Purchase Now</a></div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
                 </li>
                 <li data-transition="fade"> <img src="style/images/art/slider-bg3.jpg"  alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" />
                     <div class="tp-caption large sfr" data-x="30" data-y="263" data-speed="900" data-start="800" data-easing="Sine.easeOut">
@@ -81,9 +81,10 @@
                     </div>
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">
                         Исходный материал поставляется из Великобритании
-                        <div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Purchase Now</a></div>
+                    </div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
                 </li>
+
                 <li data-transition="fade"> <img src="style/images/art/slider-bg4.jpg"  alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" />
                     <div class="tp-caption large sfr" data-x="30" data-y="263" data-speed="900" data-start="800" data-easing="Sine.easeOut">
                         Сертификация ISO 9001 : 2000
@@ -91,7 +92,7 @@
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">
                         Имеем международный сертификат качества по ISO 9001: 2000
                     </div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Purchase Now</a></div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
                 </li>
                 <li data-transition="fade"> <img src="style/images/art/slider-bg5.jpg"  alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" />
                     <div class="tp-caption large sfr" data-x="30" data-y="263" data-speed="900" data-start="800" data-easing="Sine.easeOut">
@@ -100,22 +101,23 @@
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">
                         Производим около 8,5 миллионов инкубационных яиц
                     </div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Purchase Now</a></div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
                 </li>
             </ul>
             <div class="tp-bannertimer tp-bottom"></div>
         </div>
         <!-- /.tp-fullscreen-container -->
     </div><!-- /.revolution -->
+
     <div class="container inner">
         <div class="headline text-center">
             <h2>Первоклассные индюшки и индюшачьи яйца!</h2>
             <p class="lead animated-text letters type">
                 <span>Мы занимаемся распространением</span>
                 <span class="animated-text-wrapper">
-                                                                        <b class="is-hidden" style="opacity: 1;"><i class="out">с</i><i class="out">у</i><i class="out">т</i><i class="out">о</i><i class="out">ч</i><i class="out">н</i><i class="out">ы</i><i class="out">х</i><i class="out"> </i><i class="out">и</i><i class="out">н</i><i class="out">д</i><i class="out">ю</i><i class="out">ш</i><i class="out">а</i><i class="out">т</i></b>
-                                                                                                <b style="opacity: 1;" class="is-hidden"><i class="out">и</i><i class="out">н</i><i class="out">к</i><i class="out">у</i><i class="out">б</i><i class="out">а</i><i class="out">ц</i><i class="out">и</i><i class="out">о</i><i class="out">н</i><i class="out">н</i><i class="out">ы</i><i class="out">х</i><i class="out"> </i><i class="out">я</i><i class="out">и</i><i class="out">ц</i></b>
-                                                            </span>
+  <b class="is-hidden" style="opacity: 1;"><i class="out">с</i><i class="out">у</i><i class="out">т</i><i class="out">о</i><i class="out">ч</i><i class="out">н</i><i class="out">ы</i><i class="out">х</i><i class="out"> </i><i class="out">и</i><i class="out">н</i><i class="out">д</i><i class="out">ю</i><i class="out">ш</i><i class="out">а</i><i class="out">т</i></b>
+ <b style="opacity: 1;" class="is-hidden"><i class="out">и</i><i class="out">н</i><i class="out">к</i><i class="out">у</i><i class="out">б</i><i class="out">а</i><i class="out">ц</i><i class="out">и</i><i class="out">о</i><i class="out">н</i><i class="out">н</i><i class="out">ы</i><i class="out">х</i><i class="out"> </i><i class="out">я</i><i class="out">и</i><i class="out">ц</i></b>
+  </span>
             </p>
         </div>
         <div class="divide30"></div>
@@ -147,7 +149,7 @@
                 <!-- /.headline -->
                 <div class="divide30"></div>
                 <figure>
-                    <img src="style/images/art/hygge.png" alt="" />
+                    <img style="width: auto;" src="style/images/art/branko.png" alt="" />
                 </figure>
             </div>
             <!-- /.thin -->
@@ -165,88 +167,99 @@
             <div class="cbp-panel">
                 <div id="filters-container" class="cbp-filter-container text-center">
                     <div data-filter="*" class="cbp-filter-item-active cbp-filter-item"> Все </div>
-                    <div data-filter=".print" class="cbp-filter-item"> Обеспеченье </div>
-                    <div data-filter=".web" class="cbp-filter-item"> Продукция</div>
+                    <div data-filter=".provision" class="cbp-filter-item"> Обеспеченье </div>
+                    <div data-filter=".products" class="cbp-filter-item"> Продукция</div>
                 </div>
                 <div id="grid-container" class="cbp">
-                    <div class="cbp-item print motion"> <a href="ajax/project1.html" class="cbp-caption cbp-singlePageInline">
-                            <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p1.jpg" alt="" /> </div>
-                            <div class="cbp-caption-activeWrap">
-                                <div class="cbp-l-caption-alignCenter">
-                                    <div class="cbp-l-caption-body">
-                                        <div class="cbp-l-caption-title">Malesuada Parturient</div>
-                                        <div class="cbp-l-caption-desc">Print, Motion</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a> </div>
-                    <!--/.cbp-item -->
-                    <div class="cbp-item web logo"> <a href="ajax/project2.html" class="cbp-caption cbp-singlePageInline">
-                            <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p2.jpg" alt="" /> </div>
-                            <div class="cbp-caption-activeWrap">
-                                <div class="cbp-l-caption-alignCenter">
-                                    <div class="cbp-l-caption-body">
-                                        <div class="cbp-l-caption-title">Tellus Nibh</div>
-                                        <div class="cbp-l-caption-desc">Web Design</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a> </div>
-                    <!--/.cbp-item -->
-                    <div class="cbp-item print motion"> <a href="ajax/project3.html" class="cbp-caption cbp-singlePageInline">
-                            <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p3.jpg" alt="" /> </div>
-                            <div class="cbp-caption-activeWrap">
-                                <div class="cbp-l-caption-alignCenter">
-                                    <div class="cbp-l-caption-body">
-                                        <div class="cbp-l-caption-title">Pellentesque Mattis</div>
-                                        <div class="cbp-l-caption-desc">Print, Motion</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a> </div>
-                    <!--/.cbp-item -->
-                    <div class="cbp-item web print"> <a href="ajax/project4.html" class="cbp-caption cbp-singlePageInline">
+
+                    <div class="cbp-item provision"> <a class="cbp-caption fancybox-media" data-rel="portfolio" href="style/images/art/p4.jpg">
                             <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p4.jpg" alt="" /> </div>
                             <div class="cbp-caption-activeWrap">
                                 <div class="cbp-l-caption-alignCenter">
                                     <div class="cbp-l-caption-body">
-                                        <div class="cbp-l-caption-title">Euismod Pharetra</div>
-                                        <div class="cbp-l-caption-desc">Web Design, Print</div>
+                                        <div class="cbp-l-caption-title">Увеличить</div>
                                     </div>
                                 </div>
                             </div>
-                        </a> </div>
+                            <!--/.cbp-caption-activeWrap -->
+                        </a>
+                        <!--/.box -->
+                    </div>
                     <!--/.cbp-item -->
-                    <div class="cbp-item motion logo"> <a href="ajax/project5.html" class="cbp-caption cbp-singlePageInline">
+                    <div class="cbp-item provision"> <a class="cbp-caption fancybox-media" data-rel="portfolio" href="style/images/art/p5.jpg">
                             <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p5.jpg" alt="" /> </div>
                             <div class="cbp-caption-activeWrap">
                                 <div class="cbp-l-caption-alignCenter">
                                     <div class="cbp-l-caption-body">
-                                        <div class="cbp-l-caption-title">Fringilla Nullam</div>
-                                        <div class="cbp-l-caption-desc">Motion</div>
+                                        <div class="cbp-l-caption-title">Увеличить</div>
                                     </div>
                                 </div>
                             </div>
-                        </a> </div>
+                            <!--/.cbp-caption-activeWrap -->
+                        </a>
+                        <!--/.box -->
+                    </div>
                     <!--/.cbp-item -->
-                    <div class="cbp-item print motion"> <a href="ajax/project6.html" class="cbp-caption cbp-singlePageInline">
+                    <div class="cbp-item products"> <a class="cbp-caption fancybox-media" data-rel="portfolio" href="style/images/art/p1.jpg">
+                            <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p1.jpg" alt="" /> </div>
+                            <div class="cbp-caption-activeWrap">
+                                <div class="cbp-l-caption-alignCenter">
+                                    <div class="cbp-l-caption-body">
+                                        <div class="cbp-l-caption-title">Увеличить</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--/.cbp-caption-activeWrap -->
+                        </a>
+                        <!--/.box -->
+                    </div>
+                    <!--/.cbp-item -->
+                    <div class="cbp-item products"> <a class="cbp-caption fancybox-media" data-rel="portfolio" href="style/images/art/p2.jpg">
+                            <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p2.jpg" alt="" /> </div>
+                            <div class="cbp-caption-activeWrap">
+                                <div class="cbp-l-caption-alignCenter">
+                                    <div class="cbp-l-caption-body">
+                                        <div class="cbp-l-caption-title">Увеличить</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--/.cbp-caption-activeWrap -->
+                        </a>
+                        <!--/.box -->
+                    </div>
+                    <!--/.cbp-item -->
+                    <div class="cbp-item products"> <a class="cbp-caption fancybox-media" data-rel="portfolio" href="style/images/art/p3.jpg">
+                            <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p3.jpg" alt="" /> </div>
+                            <div class="cbp-caption-activeWrap">
+                                <div class="cbp-l-caption-alignCenter">
+                                    <div class="cbp-l-caption-body">
+                                        <div class="cbp-l-caption-title">Увеличить</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--/.cbp-caption-activeWrap -->
+                        </a>
+                        <!--/.box -->
+                    </div>
+                    <!--/.cbp-item -->
+                    <div class="cbp-item products"> <a class="cbp-caption fancybox-media" data-rel="portfolio" href="style/images/art/p6.jpg">
                             <div class="cbp-caption-defaultWrap"> <img src="style/images/art/p6.jpg" alt="" /> </div>
                             <div class="cbp-caption-activeWrap">
                                 <div class="cbp-l-caption-alignCenter">
                                     <div class="cbp-l-caption-body">
-                                        <div class="cbp-l-caption-title">Pharetra Sem</div>
-                                        <div class="cbp-l-caption-desc">Print, Motion</div>
+                                        <div class="cbp-l-caption-title">Увеличить</div>
                                     </div>
                                 </div>
                             </div>
-                        </a> </div>
+                            <!--/.cbp-caption-activeWrap -->
+                        </a>
+                        <!--/.box -->
+                    </div>
                     <!--/.cbp-item -->
                 </div>
                 <!--/.cbp -->
                 <div class="divide30"></div>
-                <div class="text-center">
-                    <div id="loadMore-container" class=""> <a href="ajax/loadmore.html" class="cbp-l-loadMore-link btn btn-border dark"> <span class="cbp-l-loadMore-defaultText">LOAD MORE</span> <span class="cbp-l-loadMore-loadingText">LOADING...</span> <span class="cbp-l-loadMore-noMoreLoading">NO MORE WORKS</span> </a> </div>
-                </div>
+
                 <!--/.text-center -->
             </div>
             <!--/.cbp-panel -->
