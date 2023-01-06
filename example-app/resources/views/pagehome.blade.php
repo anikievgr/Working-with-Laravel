85b6b4d2789e900ad9d264b8d3e9d5262987fd39
anikievgr <anikiev.gleb.2005@yandex.ru>
2023-01-06 15:24:11 +0300

    now

--- example-app/resources/views/pagehome.blade.php
+++ example-app/resources/views/pagehome.blade.php
@@ -443,39 +443,39 @@
     <div class="light-wrapper">
         <div class="container inner">
             <div class="section-title text-center">
-                <h3>Process Model</h3>
-                <p class="lead">our process on creating awesome projects</p>
+                <h3>Модель процессов</h3>
+                <p class="lead">Наш процесс разведения потрясающих индюшек</p>
             </div>
             <div class="thin3">
                 <div class="row circle-wrapper">
                     <div class="col-sm-6 col-md-3">
                         <div class="circle blue-bg">
-                            <div class="text">1. Envisioning</div>
+                            <div class="text">1. Материал</div>
                         </div>
                     </div>
                     <!-- /column -->
                     <div class="col-sm-6 col-md-3">
                         <div class="circle red-bg">
-                            <div class="text">2. Planning</div>
+                            <div class="text">2. Контроль</div>
                         </div>
                     </div>
                     <!-- /column -->
                     <div class="col-sm-6 col-md-3">
                         <div class="circle green-bg">
-                            <div class="text">3. Development</div>
+                            <div class="text">3. Забота</div>
                         </div>
                     </div>
                     <!-- /column -->
                     <div class="col-sm-6 col-md-3">
                         <div class="circle yellow-bg">
-                            <div class="text">4. Stabilization</div>
+                            <div class="text">4. Развитие</div>
                         </div>
                     </div>
                     <!-- /column -->
                 </div>
                 <!--/.row -->
                 <div class="divide30"></div>
-                <p class="text-center">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                <p class="text-center">Наши люди - это высококвалифицированные рабочие с многолетним опытом, являющиеся специалистами в своей отрасли. Мы также работаем и с внешними специалистами. Мы участвуем в сельскохозяйственных и продовольственных ярмарках на словацком и международном рынках.</p>
             </div>
             <!--/.thin -->
         </div>
@@ -484,172 +484,45 @@
     <!-- /.light-wrapper -->
 
 
-    <div class="light-wrapper">
-        <div class="container inner">
-            <div class="section-title text-center">
-                <h3>Meet Our Team</h3>
-            </div>
-            <div class="row grid-view">
-                <div class="col-sm-3 text-center">
-                    <figure><img src="style/images/art/t1.jpg" alt="" /></figure>
-                    <h4 class="post-title">Nikolas Brooten</h4>
-                    <div class="meta">Sales Manager</div>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Praesent commodo.</p>
-                    <ul class="social">
-                        <li><a href="#"><i class="icon-s-twitter"></i></a></li>
-                        <li><a href="#"><i class="icon-s-facebook"></i></a></li>
-                        <li><a href="#"><i class="icon-s-gplus"></i></a></li>
-                    </ul>
-                </div>
-                <!-- /column -->
-                <div class="col-sm-3 text-center">
-                    <figure><img src="style/images/art/t2.jpg" alt="" /></figure>
-                    <h4 class="post-title">Coriss Ambady</h4>
-                    <div class="meta">Marketing Specialist</div>
-                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros sed posuere quam.</p>
-                    <ul class="social">
-                        <li><a href="#"><i class="icon-s-twitter"></i></a></li>
-                        <li><a href="#"><i class="icon-s-facebook"></i></a></li>
-                        <li><a href="#"><i class="icon-s-linkedin"></i></a></li>
-                    </ul>
-                </div>
-                <!-- /column -->
-                <div class="col-sm-3 text-center">
-                    <figure><img src="style/images/art/t3.jpg" alt="" /></figure>
-                    <h4 class="post-title">Barclay Widerski</h4>
-                    <div class="meta">Computer Engineer</div>
-                    <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis.</p>
-                    <ul class="social">
-                        <li><a href="#"><i class="icon-s-twitter"></i></a></li>
-                        <li><a href="#"><i class="icon-s-github"></i></a></li>
-                        <li><a href="#"><i class="icon-s-gplus"></i></a></li>
-                    </ul>
-                </div>
-                <!-- /column -->
-                <div class="col-sm-3 text-center">
-                    <figure><img src="style/images/art/t4.jpg" alt="" /></figure>
-                    <h4 class="post-title">Elsie Spear</h4>
-                    <div class="meta">Strategy Director</div>
-                    <p>Maecenas faucibus mollis interdum. Aenean eu leo quam ornare sem lacinia.</p>
-                    <ul class="social">
-                        <li><a href="#"><i class="icon-s-twitter"></i></a></li>
-                        <li><a href="#"><i class="icon-s-facebook"></i></a></li>
-                        <li><a href="#"><i class="icon-s-linkedin"></i></a></li>
-                    </ul>
-                </div>
-                <!-- /column -->
-            </div>
-            <!--/.row -->
-        </div>
-        <!--/.container -->
-    </div>
-    <!-- /.light-wrapper -->
 
     <footer class="inverse-wrapper">
         <div class="container inner">
-            <div class="row">
-                <div class="col-sm-4">
-                    <h3 class="section-title widget-title">Elsewhere</h3>
-                    <p>Vestibulum id ligula porta felis euismod semper.</p>
-                    <ul class="social">
-                        <li><a href="#"><i class="icon-s-rss"></i></a></li>
-                        <li><a href="#"><i class="icon-s-twitter"></i></a></li>
-                        <li><a href="#"><i class="icon-s-facebook"></i></a></li>
-                        <li><a href="#"><i class="icon-s-pinterest"></i></a></li>
-                        <li><a href="#"><i class="icon-s-linkedin"></i></a></li>
-                        <li><a href="#"><i class="icon-s-forrst"></i></a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h3 class="section-title widget-title">Subscribe</h3>
-                    <p> Cum sociis natoque penatibus et magnis dis parturient.</p>
-                    <div class="newsletter-wrapper">
-                        <div id="mc_embed_signup2" class="newsletter-form">
-                            <form action="http://elemisfreebies.us3.list-manage1.com/subscribe/post?u=ddc180777a163e0f9f66ee014&amp;id=056957de28" method="post" id="mc-embedded-subscribe-form2" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
-                                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL2" placeholder="Enter email" required="">
-                                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                <div style="position: absolute; left: -5000px;">
-                                    <input type="text" name="b_ddc180777a163e0f9f66ee014_056957de28" value="">
-                                </div>
-                                <div class="clear">
-                                    <input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe2" class="btn">
-                                </div>
-                            </form>
-                        </div>
-                        <!-- /#mc_embed_signup2 -->
-                    </div>
-                    <!-- /.newsletter-wrapper -->
-                </div>
-                <div class="col-sm-4">
-                    <h3 class="section-title widget-title">Tags</h3>
-                    <div class="tagcloud"> <a href="#" style="font-size: 9pt;">blogroll</a> <a href="#" style="font-size: 19pt;">daily</a> <a href="#" style="font-size: 9pt;">dialog</a> <a href="#" style="font-size: 9pt;">gallery</a> <a href="#" style="font-size: 10pt;">journal</a> <a href="#" style="font-size: 9pt;">link</a> <a href="#" style="font-size: 12pt;">motion</a> <a href="#" style="font-size: 9pt;">music</a> <a href="#" style="font-size: 20pt;">photo</a> <a href="#" style="font-size: 13pt;">professional</a> <a href="#" style="font-size: 16pt;">quotation</a> <a href="#" style="font-size: 9pt;">show</a> <a href="#" style="font-size: 15pt;">sound</a> </div>
-                </div>
-            </div>
-            <hr />
-            <div class="row">
-                <div class="col-sm-3">
-                    <div class="widget"> <img src="style/images/logo.svg" data-at2x="style/images/logo@2x.png" alt="" />
-                        <div class="divide20"></div>
-                        <p>Aenean lacinia bibendum nulla sed leo posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper metus auctor fringi. Nullam quis risus. </p>
+
+            <div class="row ">
+                <div class="col-sm-6">
+                    <div class="widget "> <img src="style/images/logo.png "  height="50" data-at2x="style/images/logo@2x.png" alt="" />
+
+                        <p>Более 45 лет занимаемся специальным сектором сельского хозяйства, разведением индейки, распространением инкубационных яиц индейки и суточных индюшат.</p>
                     </div>
                     <!-- /.widget -->
                 </div>
                 <!-- /column -->
 
-                <div class="col-sm-3">
-                    <div class="widget">
-                        <h3 class="section-title widget-title">Popular Posts</h3>
-                        <ul class="post-list">
-                            <li>
-                                <h6><a href="blog-post.html">Vivamus sagittis lacus vel augue metus</a></h6>
-                                <span class="meta date">3th Oct 2012</span> </li>
-                            <li>
-                                <h6><a href="blog-post.html">Scelerisque nisl consectetur et</a></h6>
-                                <span class="meta date">28th Sep 2012</span> </li>
-                        </ul>
-                        <!-- /.post-list -->
-                    </div>
-                    <!-- /.widget -->
-                </div>
-                <!-- /column -->
 
-                <div class="col-sm-3">
+
+                <div class="col-sm-6">
                     <div class="widget">
-                        <h3 class="section-title widget-title">Get In Touch</h3>
-                        <p>Fusce dapibus, tellus commodo, tortor mauris condimentum utellus fermentum.</p>
-                        <div class="contact-info"> <i class="icon-location"></i> Moonshine St. 14/05 Light City <br />
-                            <i class="icon-phone"></i>+00 (123) 456 78 90 <br />
-                            <i class="icon-mail"></i> <a href="first.last@email.com">first.last@email.com</a> </div>
+                        <h3 class="section-title widget-title">СВЯЗАТЬСЯ</h3>
+                        <p>ООО «Бранко Рус»</p>
+                        <div class="contact-info"> <i class="icon-location"></i> 422060, респ. Татарстан, пгт. Богатые Сабы,ул. Заводская, д. 19 <br />
+                            <i class="icon-phone"></i>+7 927 039 71 17 <br />
+                            <i class="icon-mail"></i> <a href="sales@brankorus.ru"> sales@brankorus.ru</a> </div>
                     </div>
                 </div>
                 <!-- /column -->
 
-                <div class="col-sm-3">
-                    <div class="widget">
-                        <h4 class="widget-title">Categories</h4>
-                        <ul class="circled">
-                            <li><a href="#">Web Design (21)</a></li>
-                            <li><a href="#">Photography (19)</a></li>
-                            <li><a href="#">Graphic Design (16)</a></li>
-                            <li><a href="#">Manipulation (15)</a></li>
-                            <li><a href="#">Motion Graphics (12)</a></li>
-                        </ul>
-                    </div>
-                    <!-- /.widget -->
 
-                </div>
                 <!-- /column -->
             </div>
             <!-- /.row -->
 
             <hr />
-            <p class="copyright pull-left">© 2015 Hygge. All rights reserved. Theme by <a href="http://elemisfreebies.com">elemis</a>.</p>
+            <p class="copyright pull-left">© ООО «Бранко Рус», 2021. Coded by <a href="http://it-pskov.ru/">IT Nova</a>.</p>
             <ul class="footer-menu pull-right">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Portfolio</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="/">О нас</a></li>
+                <li><a href="#">Инкубирование</a></li>
+                <li><a href="#">Контакты</a></li>
+                <li><a href="#">Войти</a></li>
             </ul>
         </div>
         <!-- .container -->
