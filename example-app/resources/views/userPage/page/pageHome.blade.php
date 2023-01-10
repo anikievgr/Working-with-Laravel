09c0bf11262adc2f8717231c2b62c2f8f11e70b8
anikievgr <anikiev.gleb.2005@yandex.ru>
2023-01-10 17:19:20 +0300

    Y

--- example-app/resources/views/userPage/page/pageHome.blade.php
+++ example-app/resources/views/userPage/page/pageHome.blade.php
@@ -21,7 +21,7 @@
                     <div class="tp-caption large sfr" data-x="30" data-y="263" data-speed="900" data-start="800" data-easing="Sine.easeOut"> Разведение индейки<br />
                         </div>
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">Более 45 лет занимаемся выращиванием индейки</div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="/contact" class="btn btn-large">Связаться</a></div>
                 </li>
                 <li data-transition="fade"> <img src="style/images/art/slider-bg2.jpg"  alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" />
                     <div class="tp-caption large sfr" data-x="30" data-y="263" data-speed="900" data-start="800" data-easing="Sine.easeOut">
@@ -30,7 +30,7 @@
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">
                         Занимаем почти 90% словацкого рынка
                     </div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="/contact" class="btn btn-large">Связаться</a></div>
                 </li>
                 <li data-transition="fade"> <img src="style/images/art/slider-bg3.jpg"  alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" />
                     <div class="tp-caption large sfr" data-x="30" data-y="263" data-speed="900" data-start="800" data-easing="Sine.easeOut">
@@ -39,7 +39,7 @@
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">
                         Исходный материал поставляется из Великобритании
                     </div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="/contact" class="btn btn-large">Связаться</a></div>
                 </li>
 
                 <li data-transition="fade"> <img src="style/images/art/slider-bg4.jpg"  alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" />
@@ -49,7 +49,7 @@
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">
                         Имеем международный сертификат качества по ISO 9001: 2000
                     </div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="/contact" class="btn btn-large">Связаться</a></div>
                 </li>
                 <li data-transition="fade"> <img src="style/images/art/slider-bg5.jpg"  alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" />
                     <div class="tp-caption large sfr" data-x="30" data-y="263" data-speed="900" data-start="800" data-easing="Sine.easeOut">
@@ -58,7 +58,7 @@
                     <div class="tp-caption medium sfr" data-x="30" data-y="403" data-speed="900" data-start="1500" data-easing="Sine.easeOut">
                         Производим около 8,5 миллионов инкубационных яиц
                     </div>
-                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="#" class="btn btn-large">Связаться</a></div>
+                    <div class="tp-caption sfr" data-x="30" data-y="470" data-speed="900" data-start="2200" data-easing="Sine.easeOut"  data-endspeed="100"><a href="/contact" class="btn btn-large">Связаться</a></div>
                 </li>
             </ul>
             <div class="tp-bannertimer tp-bottom"></div>
